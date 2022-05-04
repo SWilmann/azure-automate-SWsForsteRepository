ad6026ad85c6fdd594da9524b41439e9f128fc9f
SWilmann <stein.wilmann@nav.no>
2022-05-04 15:07:57 +0200

    Ryddet litt...

--- src/oppgave_3.ps1
+++ src/oppgave_3.ps1
@@ -25,12 +25,10 @@ $webRequest = Invoke-WebRequest -Uri http://nav-deckofcards.herokuapp.com/shuffl
 
 
 $kortstokkJson = $webRequest.Content
-
 # [{"suit":"SPADES","value":"3"},{"suit":"SPADES","value":"Q"},{"suit":"HEARTS","value":"4"},{"suit":"DIAMONDS","value":"5"},{"suit":"HEARTS","value":"6"},{"suit":"CLUBS","value":"3"},{"suit":"SPADES","value":"7"},{"suit":"CLUBS","value":"6"},{"suit":"HEARTS","value":"7"},{"suit":"DIAMONDS","value":"8"},{"suit":"HEARTS","value":"Q"},{"suit":"CLUBS","value":"K"},{"suit":"CLUBS","value":"8"},{"suit":"CLUBS","value":"J"},{"suit":"SPADES","value":"8"},{"suit":"DIAMONDS","value":"4"},{"suit":"DIAMONDS","value":"2"},{"suit":"HEARTS","value":"8"},{"suit":"HEARTS","value":"K"},{"suit":"CLUBS","value":"A"},{"suit":"SPADES","value":"5"},{"suit":"HEARTS","value":"10"},{"suit":"SPADES","value":"2"},{"suit":"HEARTS","value":"J"},{"suit":"DIAMONDS","value":"6"},{"suit":"SPADES","value":"9"},{"suit":"CLUBS","value":"4"},{"suit":"SPADES","value":"J"},{"suit":"DIAMONDS","value":"A"},{"suit":"CLUBS","value":"7"},{"suit":"CLUBS","value":"5"},{"suit":"HEARTS","value":"3"},{"suit":"DIAMONDS","value":"7"},{"suit":"CLUBS","value":"9"},{"suit":"DIAMONDS","value":"9"},{"suit":"CLUBS","value":"2"},{"suit":"SPADES","value":"4"},{"suit":"CLUBS","value":"Q"},{"suit":"SPADES","value":"6"},{"suit":"DIAMONDS","value":"K"},{"suit":"HEARTS","value":"2"},{"suit":"SPADES","value":"10"},{"suit":"DIAMONDS","value":"J"},{"suit":"SPADES","value":"K"},{"suit":"DIAMONDS","value":"3"},{"suit":"DIAMONDS","value":"10"},{"suit":"DIAMONDS","value":"Q"},{"suit":"HEARTS","value":"A"},{"suit":"HEARTS","value":"5"},{"suit":"CLUBS","value":"10"},{"suit":"SPADES","value":"A"},{"suit":"HEARTS","value":"9"}]
 
 
 $kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
-
 # suit     value
 # ----     -----
 # SPADES   8
@@ -44,7 +42,6 @@ $kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
 # DIAMONDS 3
 # ...
 
-# å skrive ut kortene i kortstokken er form for loop/iterere oppgave
 
 function kortstokkTilStreng {
     [OutputType([string])]
@@ -64,6 +61,4 @@ function kortstokkTilStreng {
 
 Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
 
-# hvorfor kommer det et komma ',' etter siste kort?
-# frivillig oppgave - kan du forbedre funksjonen 'kortTilStreng' - ikke skrive ut komma etter siste kort?
 
